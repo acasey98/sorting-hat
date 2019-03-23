10fdcec98f1567fd12e750433d5239e21b56166a
Austin Casey <thepoast@gmail.com>
2019-03-23 10:58:35 -0500

    console logs inputted name and a random house, jumbotron disappears after Get Strtd is clicked

--- main.js
+++ main.js
@@ -3,23 +3,42 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
+const houseList = ['Griffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'];
+
 
 const showForm = () =>{
     let domString = '';
     domString += `<form>`;
     domString +=    `<div class="form-group">`;
-    domString +=        `<label for="exampleInputEmail1">Student Name</label>`;
-    domString +=        `<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ron Weasley">`;
+    domString +=        `<label for="nameInputForm">Student Name</label>`;
+    domString +=        `<input type="text" class="form-control" id="nameInputForm" value='' aria-describedby="emailHelp" placeholder="Ron Weasley">`;
     domString +=    `</div>`;
-    domString +=     `<button type="submit" class="btn btn-primary">Sort</button>`;
+    domString +=     `<button type="button" class="btn btn-primary" id='sortBtn'>Sort</button>`;
     domString +=    `</form>`;
-
+    
     printToDom('lol', domString);
+
+    document.getElementById('sortBtn').addEventListener('click', assignHouse);
+    document.getElementById('nameInputForm').addEventListener('submit', assignHouse);
+    document.getElementById('jumbotron-id').innerHTML = '';
 };
+
+const assignHouse = () => {
+    let domString = '';
+    const student = document.getElementById("nameInputForm").value;
+    const house = houseList[(getRandomInt(4))];
+    console.log( student, house);
+    
+}
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
 
 
 const buttonEvents = () =>{
     document.getElementById('getStarted').addEventListener('click', showForm);
+    
 };
 
 
